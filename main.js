a5fefed998a824dfa09bb73774779fdbfe9802a1
maimai84 <maialmasalmah@gmail.com>
2018-02-16 11:02:07 +0200

    color faces of cube

--- main.js
+++ main.js
@@ -10,29 +10,36 @@ $('body').append(renderer.domElement)
 
 
 var scene = new THREE.Scene;
-var cubeGeometry = new THREE.CubeGeometry(80, 80, 80);
-var cubeMaterial = new THREE.MeshLambertMaterial({color:0x10c876 });
+var cubeGeometry = new THREE.CubeGeometry(100, 100, 100);
+cubeGeometry.faces[0].color.setHex(Math.random()*0xf0c876);
+cubeGeometry.faces[1].color.setHex(Math.random()*0xf0c876);
+cubeGeometry.faces[2].color.setHex(Math.random()*0xf0c876);
+cubeGeometry.faces[3].color.setHex(Math.random()*0xf0c876);
+cubeGeometry.faces[4].color.setHex(Math.random()*0xf0c876);
+cubeGeometry.faces[5].color.setHex(Math.random()*0xf0c876);
+
+
+
+
+// cubeGeometry.faces[0].color.setHex( Math.random() * 0xffffff );
+// cubeGeometry.faces[1].color.setHex( Math.random() * 0xffffff );
+// cubeGeometry.faces[2].color.setHex( Math.random() * 0xffffff );
+// cubeGeometry.faces[3].color.setHex( Math.random() * 0xffffff );
+// cubeGeometry.faces[4].color.setHex( Math.random() * 0xffffff );
+// cubeGeometry.faces[5].color.setHex( Math.random() * 0xffffff );
+
+
+var cubeMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff, vertexColors: THREE.FaceColors } );
 var cube = new THREE.Mesh(cubeGeometry,cubeMaterial);
 scene.add(cube);
 
 
 var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000)
-camera.position.y = 160;
+camera.position.y = 170;
 camera.position.z = 400;
 scene.add(camera);
 camera.lookAt(cube.position);
 
-var skyboxGeometry = new THREE.CubeGeometry(10000, 10000, 10000);
-var skyboxMaterial = new THREE.MeshBasicMaterial({ color: 0x000000, side: THREE.BackSide });
-var skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
- 
-scene.add(skybox);
-
-var pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(0, 100, 200);
- 
-
-scene.add(pointLight);
 
 
 renderer.render(scene, camera);

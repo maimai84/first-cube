6995bfef0e1ed97d3dd2f160dfca29b5036e9430
maimai84 <maialmasalmah@gmail.com>
2018-02-16 16:28:08 +0200

    move the cube

--- main.js
+++ main.js
@@ -20,8 +20,6 @@ cubeGeometry.faces[5].color.setHex(Math.random()*0xf0c876);
 
 
 
-
-
 // cubeGeometry.faces[0].color.setHex( Math.random() * 0xffffff );
 // cubeGeometry.faces[1].color.setHex( Math.random() * 0xffffff );
 // cubeGeometry.faces[2].color.setHex( Math.random() * 0xffffff );
@@ -33,9 +31,9 @@ cubeGeometry.faces[5].color.setHex(Math.random()*0xf0c876);
 var cubeMaterial = new THREE.MeshPhongMaterial( { color: 0xffffff, vertexColors: THREE.FaceColors } );
 var cube = new THREE.Mesh(cubeGeometry,cubeMaterial);
 
-
-
  scene.add(cube);
+
+
 // var pointLight = new THREE.PointLight(0xffffff);
 //  pointLight.position.set(0,110, 310);
  
@@ -44,15 +42,37 @@ var cube = new THREE.Mesh(cubeGeometry,cubeMaterial);
 
 var light = new THREE.AmbientLight( 0x404040 ); // soft white light
 scene.add( light );
+
 var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.7 );
 scene.add( directionalLight );
+
+
 
 var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000)
 camera.position.y =360 ;
 camera.position.z = 360;
 scene.add(camera);
 camera.lookAt(cube.position);
+var xSpeed = 0.001;
+var ySpeed = 0.001;
 
+document.addEventListener("keydown", onDocumentKeyDown, false);
+function onDocumentKeyDown(event) {
+    var keyCode = event.which;
+    if (keyCode == 87) {
+        cube.position.z -= 40;
+    } else if (keyCode === 83) {
+        cube.position.z += 40;
+    } else if (keyCode === 65) {
+        cube.position.x -= 40;
+    } else if (keyCode === 68) {
+        cube.position.x += 40;
+     } else if (keyCode === 32) {
+         cube.position.set(0,0,0);
+    
+
+     }
+};
 
 
 
@@ -62,15 +82,15 @@ var planeGeometry = new THREE.PlaneBufferGeometry( 500, 500, 10, 10 );
   plane.position.set(30, -20, 0);
   plane.receiveShadow = true;
   plane.rotation.x = -90 * Math.PI / 180;
-
-
   scene.add(plane);
+
 
 var skyboxGeometry = new THREE.CubeGeometry(10000, 10000, 10000);
  var skyboxMaterial = new THREE.MeshBasicMaterial({ color: 0xffccff, side: THREE.BackSide });
  var skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
  
 scene.add(skybox);
+
 
 function render(){
 	renderer.render(scene, camera);

ebad5bccccd98158ca8e61249a79ab3f9c73388f
maimai84 <maialmasalmah@gmail.com>
2018-02-16 20:08:34 +0200

    shader

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var height=window.innerHeight;
 var renderer=new THREE.WebGLRenderer({antialias:true});
 renderer.setSize(width,height);
 $('body').append(renderer.domElement)
-
+ renderer.shadowMapEnabled = true;
 
 
 var scene = new THREE.Scene;
@@ -28,50 +28,62 @@ cubeGeometry.faces[5].color.setHex(Math.random()*0xf0c876);
 // cubeGeometry.faces[5].color.setHex( Math.random() * 0xffffff );
 
 
-var cubeMaterial = new THREE.MeshPhongMaterial( { color: 0xffffff, vertexColors: THREE.FaceColors } );
+var cubeMaterial = new THREE.MeshPhongMaterial( { color: 0xffccff, vertexColors: THREE.FaceColors } );
 var cube = new THREE.Mesh(cubeGeometry,cubeMaterial);
-
+cube.castShadow = true;
  scene.add(cube);
 
 
-// var pointLight = new THREE.PointLight(0xffffff);
-//  pointLight.position.set(0,110, 310);
- 
+ // var pointLight = new THREE.PointLight(0xffffff);
+ //  pointLight.position.set(0,110, 900);
 
-//  scene.add(pointLight);
+ // scene.add(pointLight);
+var spotLight = new THREE.SpotLight(0xffffff);
+spotLight.position.set(155, 130,330);
+spotLight.castShadow = true;
+scene.add(spotLight);
 
 var light = new THREE.AmbientLight( 0x404040 ); // soft white light
 scene.add( light );
 
-var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.7 );
-scene.add( directionalLight );
+// var directionalLight = new THREE.DirectionalLight( 0xffccff, 0.8 );
+
+// scene.add( directionalLight );
 
 
 
 var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000)
 camera.position.y =360 ;
-camera.position.z = 360;
+camera.position.z = 330;
+
+
 scene.add(camera);
 camera.lookAt(cube.position);
-var xSpeed = 0.001;
-var ySpeed = 0.001;
+
 
 document.addEventListener("keydown", onDocumentKeyDown, false);
 function onDocumentKeyDown(event) {
     var keyCode = event.which;
     if (keyCode == 87) {
-        cube.position.z -= 40;
+    	(cube.rotation.z=Math.PI *90/180)&&
+        (cube.position.z -= 40);
     } else if (keyCode === 83) {
-        cube.position.z += 40;
+    	(cube.rotation.z=Math.PI *90/180)&&
+        (cube.position.z += 40);
     } else if (keyCode === 65) {
-        cube.position.x -= 40;
+    	(cube.rotation.x=Math.PI *90/180)&&
+        (cube.position.x -= 40);
     } else if (keyCode === 68) {
-        cube.position.x += 40;
-     } else if (keyCode === 32) {
+    	(cube.rotation.x=Math.PI *90/180)&&
+        (cube.position.x += 40);
+     } else if (keyCode === 13) {
+
          cube.position.set(0,0,0);
     
 
      }
+
+
 };
 
 
@@ -94,7 +106,8 @@ scene.add(skybox);
 
 function render(){
 	renderer.render(scene, camera);
-	  
+	  // cube.rotation.z=Math.PI *90/180;
+   //   cube.rotation.x=Math.PI *90/180;
     requestAnimationFrame(render);
 
 
